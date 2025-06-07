72341742f919ea9e8f4550a2014c6c5e5b73fdbf
husein-shkrijelj <husein.shkrijelj@gmail.com>
2025-06-07 17:29:04 +0200

    fix: rename service

---
+++ Application/Interfaces/IImageService.cs
@@ -0,0 +1,12 @@
+﻿using ImageApi.Application.DTOs;
+using Microsoft.AspNetCore.Http;
+using ImageApi.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+public interface IImageService
+{
+    Task<ImageUploadResultDto> UploadImageAsync(IFormFile file);
+    Task<string?> GetResizedImagePathAsync(string imageId, int height);
+    Task<IEnumerable<ImageInfo>> GetAllImagesAsync();
+}
